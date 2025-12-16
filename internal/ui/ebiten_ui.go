507323725620d1fcad952f97cee136b34cbb61e5
Harris van Weert <harrisvanweert@gmail.com>
2025-12-16 22:55:35 +0100

    Removed line limniting only 30 ants on screen

--- internal/ui/ebiten_ui.go
+++ internal/ui/ebiten_ui.go
@@ -17,8 +17,5 @@ func Draw(screen *ebiten.Image, state *game.GameState, ants []assets.AntSprite) 
 	text.Draw(screen, fmt.Sprintf("Ants: %d", state.Workers), basicfont.Face7x13, 20, 70, color.White)
 	text.Draw(screen, fmt.Sprintf("Food: %d", state.Food), basicfont.Face7x13, 20, 90, color.White)
 	text.Draw(screen, fmt.Sprintf("Eggs: %d", state.Eggs), basicfont.Face7x13, 20, 110, color.White)
-
-	if state.Workers < 30 {
-		assets.DrawAnts(screen, ants)
-	} // now types match
+	assets.DrawAnts(screen, ants)
 }
